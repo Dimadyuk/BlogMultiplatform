62cb904e8fd9bd298998ec9fb3be0f16c2d28364
Dzmitry Dzemidziuk <extern.dzemidziuk_dzmitry@allianz.com>
2024-12-08 22:27:03 +0100

    added login screen UI

---
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.blogmultiplatform.utils
+
+object Constants {
+    const val FONT_FAMILY = "Roboto"
+}
+
+object Res {
+    object Image {
+        const val LOGO = "/logo.svg"
+    }
+}
