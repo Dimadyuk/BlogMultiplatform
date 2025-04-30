0839aa1819844ed52c69aab07af0df7ecec76c1e
Dzmitry Dzemidziuk <extern.dzemidziuk_dzmitry@allianz.com>
2025-04-30 09:41:13 +0200

    implemented popular posts UI

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/Index.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/Index.kt
@@ -21,52 +21,14 @@ import com.example.blogmultiplatform.utils.fetchLatestPosts
 import com.example.blogmultiplatform.utils.fetchMainPosts
 import com.example.blogmultiplatform.utils.fetchPopularPosts
 import com.example.blogmultiplatform.utils.fetchSponsoredPosts
-import com.varabyte.kobweb.compose.css.StyleVariable
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.graphics.Color
-import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
-import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
-import com.varabyte.kobweb.compose.ui.modifiers.boxShadow
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
-import com.varabyte.kobweb.compose.ui.modifiers.gap
-import com.varabyte.kobweb.compose.ui.modifiers.height
-import com.varabyte.kobweb.compose.ui.modifiers.margin
-import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.base
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.web.css.cssRem
-import org.jetbrains.compose.web.css.vh
-
-// Container that has a tagline and grid on desktop, and just the tagline on mobile
-val HeroContainerStyle = CssStyle {
-    base { Modifier.fillMaxWidth().gap(2.cssRem) }
-    Breakpoint.MD { Modifier.margin { top(20.vh) } }
-}
-
-// A demo grid that appears on the homepage because it looks good
-val HomeGridStyle = CssStyle.base {
-    Modifier
-        .gap(0.5.cssRem)
-        .width(70.cssRem)
-        .height(18.cssRem)
-}
-
-private val GridCellColorVar by StyleVariable<Color>()
-val HomeGridCellStyle = CssStyle.base {
-    Modifier
-        .backgroundColor(GridCellColorVar.value())
-        .boxShadow(blurRadius = 0.6.cssRem, color = GridCellColorVar.value())
-        .borderRadius(1.cssRem)
-}
-
 
 @Page
 @Composable
@@ -123,6 +85,10 @@ fun HomePage() {
             onSuccess = {
                 if (it is ApiListResponse.Success) {
                     popularPosts.addAll(it.data)
+                    popularPostsToSkip += Constants.POSTS_PER_PAGE
+                    if (it.data.size >= Constants.POSTS_PER_PAGE) {
+                        showMorePopular = true
+                    }
                 }
                 println("Popular posts - $it")
             },
@@ -187,6 +153,36 @@ fun HomePage() {
         SponsoredPostsSection(
             breakpoint = breakpoint,
             posts = sponsoredPosts,
+            onClick = {
+
+            },
+        )
+        PostsSection(
+            breakpoint = breakpoint,
+            title = "Popular Posts",
+            posts = popularPosts,
+            showMoreVisibility = showMorePopular,
+            onShowMore = {
+                scope.launch {
+                    fetchPopularPosts(
+                        skip = popularPostsToSkip,
+                        onSuccess = { response ->
+                            if (response is ApiListResponse.Success) {
+                                if (response.data.isNotEmpty()) {
+                                    if (response.data.size < Constants.POSTS_PER_PAGE) {
+                                        showMorePopular = false
+                                    }
+                                    popularPosts.addAll(response.data)
+                                    popularPostsToSkip += Constants.POSTS_PER_PAGE
+                                } else {
+                                    showMorePopular = false
+                                }
+                            }
+                        },
+                        onError = { }
+                    )
+                }
+            },
             onClick = {
 
             },
